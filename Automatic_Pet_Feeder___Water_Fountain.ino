eea3b0b2f0541d44bbb102ee76d38e5270404887
S3C <84039121+S3C95@users.noreply.github.com>
2022-01-06 23:54:00 +0800

    Update Automatic_Pet_Feeder___Water_Fountain.ino

--- Automatic_Pet_Feeder___Water_Fountain.ino
+++ Automatic_Pet_Feeder___Water_Fountain.ino
@@ -89,7 +89,7 @@ void loop() {
         Servo1.write(pos); // tell servo to go to position in variable 'pos'
         delay(15); // waits 15ms for the servo to reach the position
         Serial.print("START = ");
-        Serial.println(pos)
+        Serial.println(pos);
       }
       for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
         Servo1.write(pos); // tell servo to go to position in variable 'pos'
